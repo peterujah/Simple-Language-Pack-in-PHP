368604569ab6b97c9838d4e00f78b4a4d3e73602
Chigozie peter <ujahchigozie@gmail.com>
2019-02-21 15:05:31 +0800

    Create language_fr.php

---
+++ language_fr.php
@@ -0,0 +1,10 @@
+<?php
+$lang = array(
+    "name" => "French",
+    "code" => "fr",
+	"title" => "Test de mon pack de langue PHP",
+	"message" => "Un moyen simple de créer un pack de langue en php par Peter Ujah",
+	"error_login" => "Mot de passe incorrect"
+	/*ETC ...........*/
+);
+?>
