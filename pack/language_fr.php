c5bda8e8700bfa2ace98432fd0cff6f099ffae96
Chigozie peter <ujahchigozie@gmail.com>
2019-02-21 15:15:33 +0800

    Create language_fr.php

---
+++ pack/language_fr.php
@@ -0,0 +1,19 @@
+<?php
+/*
+	* How To Make a PHP Language Pack 
+	* Created by Peter C.U on 21/02/2019 .
+	* SourceJail @peterujah
+	* GitHub @peterujah
+	* IG @peterval_ujah
+	* I will give two example English and French
+	* First create a php file call "language_en.php" and "language_fr.php"
+*/
+$lang = array(
+    "name" => "French",
+    "code" => "fr",
+	"title" => "Test de mon pack de langue PHP",
+	"message" => "Un moyen simple de créer un pack de langue en php par Peter Ujah",
+	"error_login" => "Mot de passe incorrect"
+	/*ETC ...........*/
+);
+?>
