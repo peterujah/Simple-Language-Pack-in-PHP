6e7bb86641a40b428fedeeff0d6dc4ffb0569749
Chigozie peter <ujahchigozie@gmail.com>
2019-02-21 15:04:57 +0800

    Create language_en.php

---
+++ language_en.php
@@ -0,0 +1,10 @@
+<?php
+$lang = array(
+	"name" => "English",
+	"code" => "en",
+	"title" => "Testing My PHP Language Pack",
+	"message" => "Simple way of creating a language pack in php by Peter Ujah",
+	"error_login" => "invalid password"
+	/*ETC ...........*/
+);
+?>
